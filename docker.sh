a9aa82a5a05ae39e39348746dea1081035d133b3
dogeggly <964988862@qq.com>
2026-07-11 15:35:32 +0800

    docx: 新增脚本文件

--- docker.sh
+++ docker.sh
@@ -5,3 +5,10 @@ docker run -d \
   -e POSTGRES_PASSWORD=123456 \
   -v /data/pg17-vector:/var/lib/postgresql/data \
   pgvector/pgvector:pg17
+
+docker run -d --name emqx -p 1883:1883 -p 18083:18083 emqx:latest
+
+mvn -version
+mvn package -DskipTests
+java -version
+java -jar target\smart-street-light-0.0.1-SNAPSHOT.jar
